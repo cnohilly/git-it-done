fbeb9577b7a935ee946b9d4dd8a097d971e5d857
ctnohilly <ctnohilly@gmail.com>
2022-06-08 14:52:11 -0400

    Added buttons for featured repos for languages

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector('#language-buttons');
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -50,11 +51,11 @@ var displayRepos = function (repos, searchTerm) {
     for (var i = 0; i < repos.length; i++) {
         // format repo name
         var repoName = repos[i].owner.login + "/" + repos[i].name;
-        
+
         // create a container for each repo
         var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
-        repoEl.setAttribute("href", "./single-repo.html?repo="+repoName);
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
         // create a span element to hold repository name
         var titleEl = document.createElement("span");
         titleEl.textContent = repoName;
@@ -84,4 +85,29 @@ var displayRepos = function (repos, searchTerm) {
     console.log(searchTerm);
 }
 
+var getFeaturedRepos = function (language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                displayRepos(data.items, language);
+            });
+        } else {
+            alert("Error: GitHub User Not Found");
+        }
+    });
+};
+
+var buttonClickHandler = function (event) {
+    var language = event.target.getAttribute('data-language');
+    if (language) {
+        getFeaturedRepos(language);
+
+        // clear old content
+        repoContainerEl.textContent = "";
+    }
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
